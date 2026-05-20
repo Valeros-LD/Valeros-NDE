1fa7960f66d044e9c71cc600eaec0fceae534498
Simon Dirks <mail@simondirks.com>
2026-05-20 15:40:01 +0200

    Add initial config page with facets order drag and drop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Route } from '@angular/router';
 import { DetailsComponent } from './features/details/components/details-page/details.component';
 import { SearchPageComponent } from './features/search/components/search-page/search-page.component';
+import { ConfigPageComponent } from './features/config/config-page.component';
 
 export const appRoutes: Route[] = [
   {
     path: 'details',
     component: DetailsComponent,
+  },
+  {
+    path: 'config',
+    component: ConfigPageComponent,
   },
   {
     path: '**',
