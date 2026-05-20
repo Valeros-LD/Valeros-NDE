a115a3cd5ced56b6b9cd842f6ac54f1f9ce58f6b
Simon Dirks <mail@simondirks.com>
2026-05-20 15:44:19 +0200

    Navigate to config page from button in header

--- src/app/shared/header-banner/header-banner.component.ts
+++ src/app/shared/header-banner/header-banner.component.ts
@@ -1,15 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { featherHelpCircle, featherSettings } from '@ng-icons/feather-icons';
 
 @Component({
   selector: 'app-header-banner',
   templateUrl: './header-banner.component.html',
+  imports: [NgIcon],
+  viewProviders: [provideIcons({ featherSettings })],
 })
 export class HeaderBannerComponent {
   private router = inject(Router);
 
   goToHome(): void {
     this.router.navigate(['/search'], {
+      queryParams: {},
+    });
+  }
+
+  goToConfig(): void {
+    this.router.navigate(['/config'], {
       queryParams: {},
     });
   }
