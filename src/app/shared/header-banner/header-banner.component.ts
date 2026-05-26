74954ef60272d9684a237f29d0914c0b178ae519
Simon Dirks <mail@simondirks.com>
2026-05-26 16:17:32 +0200

    Add link to GH

--- src/app/shared/header-banner/header-banner.component.ts
+++ src/app/shared/header-banner/header-banner.component.ts
@@ -1,13 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { featherHelpCircle, featherSettings } from '@ng-icons/feather-icons';
+import {
+  featherHelpCircle,
+  featherSettings,
+  featherGithub,
+} from '@ng-icons/feather-icons';
 
 @Component({
   selector: 'app-header-banner',
   templateUrl: './header-banner.component.html',
   imports: [NgIcon],
-  viewProviders: [provideIcons({ featherSettings })],
+  viewProviders: [provideIcons({ featherGithub })],
 })
 export class HeaderBannerComponent {
   private router = inject(Router);
