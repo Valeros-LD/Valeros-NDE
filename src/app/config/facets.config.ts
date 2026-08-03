62ad68ce54726a9f595d24ec44985d642d603f89
Simon Dirks <mail@simondirks.com>
2026-08-03 14:40:03 +0200

    feat: configure facets appearance

--- src/app/config/facets.config.ts
+++ src/app/config/facets.config.ts
@@ -15,10 +15,18 @@ export const FACETS_CONFIG: FacetConfig[] = [
   { name: 'genre', label: 'Genre', icon: 'tag' },
   { name: 'material', label: 'Materiaal', icon: 'package' },
   {
+    name: 'type',
+    label: 'Soort',
+    icon: 'grid',
+  },
+  {
     name: 'additionalType',
     label: 'Soort (aanvullend)',
     icon: 'grid',
   },
   { name: 'publisher', label: 'Uitgever', icon: 'users' },
   { name: 'license', label: 'Licentie', icon: 'file-text' },
+  { name: 'about', label: 'Over', icon: 'info' },
+  { name: 'hasMedia', label: 'Met afbeelding', icon: 'image' },
+  { name: 'isPartOf', label: '', hidden: true },
 ];
