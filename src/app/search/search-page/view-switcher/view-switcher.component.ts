5da94ee4198aa4d8bcdf14c3a9dee538f4ce8f01
Simon Dirks <mail@simondirks.com>
2026-08-19 12:14:39 +0200

    feat: reset page size on view change

--- src/app/search/search-page/view-switcher/view-switcher.component.ts
+++ src/app/search/search-page/view-switcher/view-switcher.component.ts
@@ -31,7 +31,7 @@ export class ViewSwitcherComponent {
   onViewChange(viewType: ViewType): void {
     this.router.navigate([], {
       relativeTo: this.route,
-      queryParams: { view: viewType },
+      queryParams: { view: viewType, pageSize: null },
       queryParamsHandling: 'merge',
     });
   }
